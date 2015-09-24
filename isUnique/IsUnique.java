13c4812100e01a7e2f0e2259b532fe560aeaf130
Hesham Salman <hesham8@gmail.com>
2015-09-24 02:50:12 -0400

    Included Poor Solutions
    Having great solutions is, well, great. Having bad ones can be helpful
    as well because it prompts the discussion of WHY they’re bad.

--- isUnique/IsUnique.java
+++ isUnique/IsUnique.java
@@ -1,5 +1,6 @@
 import java.util.Set;
 import java.util.HashSet;
+
 public class IsUnique {
 
   /*With use of additional data structures
@@ -10,7 +11,6 @@ public class IsUnique {
     Space Complexity is O(1).
    */
   public boolean isUnique(String s) {
-
     //ExASCII = 256. Standard ASCII = 128. UTF-8 = ???
     if (s.length() > 256) {
       return false;
@@ -53,4 +53,51 @@ public class IsUnique {
     }
     return true;
   }
+
+  /*Without the use of additonal data structures.
+    Sorts the underlying character array of the string, iterates through it and
+    compares adjacent characters.
+
+    Time complexity: O(nlogn). Arguably, O(n^2) if the java quick-sort hits its
+    worst-case time (highly unlikely).
+    Space complexity: Arguably, O(1), because the largest the array will ever be
+    is the size of the character set. Otherwise, O(n), where n is the size of
+    the input string.
+   */
+  public boolean badSolution(String s) {
+    if (s.length() > 256) {
+      return false;
+    }
+    char[] charArray = s.toCharArray();
+    java.util.Arrays.sort(charArray);
+
+    for (int i=0, j=i+1; i < charArray.length - 1; i++, j++) {
+      if (charArray[i] == charArray[j]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /*This solution is terri-bad, but it works. Does not require additional data
+    structures.
+
+    Time complexity: O(n^2)
+    Space complexity: O(1)/O(n).
+   */
+  public boolean worseSolution(String s) {
+    if (s.length() > 256) {
+      return false;
+    }
+
+    for (int i=0; i < s.length() - 1; i++) {
+      for (int j=i+1; j < s.length(); j++) {
+        if (s.charAt(i) == s.charAt(j)) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
 }
