c4fe2b7c93cbd3544bfd0fbb96eb11d946a3940e
Hesham Salman <hesham8@gmail.com>
2016-08-21 00:58:31 -0400

    RemoveDups_WithSet

---
+++ linked_lists/src/RemoveDups/RemoveDuplicates.java
@@ -0,0 +1,45 @@
+package RemoveDups;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by heshamsalman on 8/21/16.
+ */
+public class RemoveDuplicates {
+
+    Node removeDuplicateNodes(Node head) {
+        Set<Integer> dataSet = new HashSet<>();
+        Node n = head;
+        dataSet.add(head.data);
+        while (n.next != null) {
+            if (dataSet.contains(n.next.data)) {
+                n.next = n.next.next;
+            } else {
+                n = n.next;
+                if (n != null)
+                    dataSet.add(n.data);
+            }
+        }
+        return head;
+    }
+}
+
+class Node {
+    Node next;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+    }
+
+    void appendToEnd(int k) {
+        Node n = this;
+        Node newNode = new Node(k);
+
+        while(n.next != null)
+            n = n.next;
+
+        n.next = newNode;
+    }
+}
---
+++ linked_lists/src/RemoveDups/RemoveDuplicatesTest.java
@@ -0,0 +1,65 @@
+package RemoveDups;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by heshamsalman on 8/21/16.
+ */
+public class RemoveDuplicatesTest {
+    RemoveDuplicates rD = new RemoveDuplicates();
+    Node head = new Node(1);
+
+    @Test
+    public void removeDuplicateNodes_SeriesOfDuplicates() {
+        head.next = new Node(1);
+        head.next.next = new Node(1);
+        head.next.next.next = new Node(1);
+        head.next.next.next.next = new Node(1);
+
+        Node n = rD.removeDuplicateNodes(head);
+        int expected = 0;
+        int actual = countNodeDepth(n);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeDuplicateNodes_ScatteredDuplicates() {
+        head.next = new Node(1);
+        head.next.next = new Node(0);
+        head.next.next.next = new Node(1);
+        head.next.next.next.next = new Node(3);
+        head.next.next.next.next.next = new Node(6);
+        head.next.next.next.next.next.next = new Node(4);
+        Node n = rD.removeDuplicateNodes(head);
+        int expected = 4;
+        int actual = countNodeDepth(n);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeDuplicateNodes_FinalDuplicate() {
+        head.next = new Node(1);
+        head.next.next = new Node(0);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(3);
+        head.next.next.next.next.next = new Node(6);
+        head.next.next.next.next.next.next = new Node(1);
+        Node n = rD.removeDuplicateNodes(head);
+        int expected = 4;
+        int actual = countNodeDepth(n);
+        assertEquals(expected, actual);
+    }
+
+    int countNodeDepth(Node head) {
+        int count = 0;
+        Node n = head;
+        while (n.next != null) {
+            count++;
+            n = n.next;
+        }
+        return count;
+    }
+
+}
