1537952b42a3b5e0b0329c102a3273d06aab3319
Hesham Salman <hesham8@gmail.com>
2016-08-20 22:08:13 -0400

    Began writing frame of the problem

---
+++ Chapter One/src/ZeroMatrix/ZeroMatrix.java
@@ -0,0 +1,46 @@
+package ZeroMatrix;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by heshamsalman on 8/20/16.
+ */
+public class ZeroMatrix {
+
+    class Point {
+        int outer; // int[]
+        int inner; // int[][]
+
+        Point(int outer, int inner) {
+            this.outer = outer;
+            this.inner = inner;
+        }
+    }
+
+    int[][] zeroMatrix(int[][] matrix) {
+        List<Point> pointList = findPoints(matrix);
+
+        for (Point p : pointList) {
+            for (int i = 0; i < p.outer; i++) {
+                matrix[i][p.inner] = 0;
+            }
+
+            for (int i = 0; i < p.inner; i++) {
+                matrix[p.outer][i] = 0;
+            }
+        }
+        return matrix;
+    }
+
+    List<Point> findPoints(int[][] matrix) {
+        List<Point> pointList = new ArrayList<ZeroMatrix.Point>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] == 0)
+                    pointList.add(new Point(i, j));
+            }
+        }
+        return pointList;
+    }
+}
---
+++ Chapter One/src/ZeroMatrix/ZeroMatrixTest.java
@@ -0,0 +1,31 @@
+package ZeroMatrix;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by heshamsalman on 8/20/16.
+ */
+public class ZeroMatrixTest {
+    @Test
+    public void zeroMatrix_WhereSomethingToZero() {
+
+    }
+
+    @Test
+    public void zeroMatrix_WhereNothingToZero() {
+
+    }
+
+    @Test
+    public void findPoints_WhereNoPoints() {
+
+    }
+
+    @Test
+    public void findPoints_WherePoints() {
+
+    }
+
+}
