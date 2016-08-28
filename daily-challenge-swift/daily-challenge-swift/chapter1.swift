2511e2cf73f6b9cfb927be3bffa7c737ee406299
Hesham Salman <hesham8@gmail.com>
2016-08-27 21:23:07 -0400

    context

--- daily-challenge-swift/daily-challenge-swift/chapter1.swift
+++ daily-challenge-swift/daily-challenge-swift/chapter1.swift
@@ -9,12 +9,20 @@
 import Foundation
 
 struct ChapterOne {
+    
+    /// Is Unique:
+    /// Implement an algorithm to determine if a string has all unique characters.
+    
     // ASCII only
     func isUnique(word: String) -> Bool {
         let sizeOfCharSet = 128
         if (word.characters.count) > sizeOfCharSet { return false }
         return Set(word.characters.map { $0 }).count == word.characters.count ? true : false
     }
+    
+    /// Is Palindrome:
+    /// Given a string, write a function to check if it is a permutation of a palindrome.
+    
     // ASCII Only
     func isPalindrome(word: String) -> Bool {
         let word = word.lowercased().replacingOccurrences(of: " ", with: "")
@@ -53,15 +61,25 @@ struct ChapterOne {
         return true
     }
     
+    /// Urlify:
+    /// Write a method to replace all spaces in a string with '%20'
+    
     func urlify(word: String) -> String {
         return word.replacingOccurrences(of: " ", with: "%20")
     }
+    
+    /// Is Permutation
+    /// Given two strings, write a method to decide if one is a permutation of the other
     
     func isPermutation(ofString: String, string: String) -> Bool {
         let string = string.lowercased().replacingOccurrences(of: " ", with: ""), ofString = ofString.lowercased().replacingOccurrences(of: " ", with: "")
         if string.characters.count != ofString.characters.count { return false }
         return Set(string.characters).count == Set(ofString.characters).count
     }
+    
+    /// One Edit Away
+    /// There are three types of edits that can be performed on a string: insertion, deletion, replacement. 
+    /// Given two strings, write a method to check if they are within one edit away.
     
     func isOneEditAway(fromString: String, string: String) -> Bool {
         if abs(fromString.characters.count - string.characters.count) > 1 { return false }
@@ -79,6 +97,12 @@ struct ChapterOne {
         return true
     }
     
+    /// String Compression:
+    /// Implement a method to perform basic string compression using the counts of repeated characters.
+    /// For example, the string `aabcccccaaa` would become `a2b1c5a3`. If the compressed string would
+    /// not be smaller than the original string, return the original string. Assume that the string only 
+    /// has uppercase and lowercase characters in it.
+    
     func compress(string: String) -> String {
         if Set(string.characters).count >= string.characters.count / 2 { return string }
         var characterCount = [Character: Int]()
@@ -88,10 +112,19 @@ struct ChapterOne {
         return string
     }
     
+    /// String Rotation: 
+    /// Assume you have a method `isSubstring` which checks if one word is a substring of another.
+    /// Given two strings, write code to check if one string is a rotation of the other using only one
+    /// call to `isSubstring`.
+    
     func stringRotation(ofString: String, string: String) -> Bool {
         let ofString = ofString + ofString
         return ofString.contains(string)
     }
+
+    /// Zero Matrix:
+    /// Write an algorithmn such that if an element in an MxN matrix is 0, its entire row and column
+    /// are set to 0.
     
     private struct Point {
         var inner: Int
